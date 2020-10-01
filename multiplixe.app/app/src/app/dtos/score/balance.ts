504b7e6f408c87cc0451e18d0f94945bce76ba3d
René Bizelli <renebizelli@gmail.com>
2020-10-01 18:53:38 -0300

    [Feature] Saldo

---
+++ multiplixe.app/app/src/app/dtos/score/balance.ts
@@ -0,0 +1,6 @@
+export class Balance {
+    public value: number = 0;
+    static create() {
+       return new Balance();
+    }
+ }
--- multiplixe.app/app/src/app/dtos/score/score.dto.ts
+++ multiplixe.app/app/src/app/dtos/score/score.dto.ts
@@ -1,3 +1,4 @@
+import { Balance } from './balance';
 import { Level } from './level';
 import { Pointing } from './pointing';
 import { SocialMedia } from './social-media'
@@ -5,6 +6,7 @@ import { SocialMedia } from './social-media'
 export class Score {
     public level: Level = Level.create();
     public pointing: Pointing = Pointing.create();
+    public balance: Balance = Balance.create();
     public socialMedias: SocialMedia[] = [];
 
     static create() {
